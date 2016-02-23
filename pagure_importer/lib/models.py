c6f38e1cb86c3abf3692e06b990efc9bdf20dcbc
Vivek Anand <vivekanand1101@gmail.com>
2016-02-23 13:52:42 +0530

    Model to represent an issue as json

---
+++ pagure_importer/lib/models.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import json
+import uuid
+
+class Issue():
+    ''' Represents an Issue '''
+
+    def __init__(
+            self, id, title, content,
+            status, date_created, user, private, tags,
+            depends, blocks, assignee, comments=None):
+
+        self.id = id
+        self.title = title
+        self.content = content
+        self.status = status
+        self.date_created = date_created
+        self.user = user
+        self.private = private
+        self.tags = tags
+        self.depends = depends
+        self.blocks = blocks
+        self.assignee = assignee
+        self.comments = comments
+        self.uid = uuid.uuid4().hex
+
+    def to_json(self):
+        ''' Returns a dictionary representation of the issue.
+
+        '''
+        output = {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content,
+            'status': self.status,
+            'date_created': self.date_created.strftime('%s'),
+            'user': self.user,
+            'private': self.private,
+            'tags': self.tags,
+            'depends': self.depends,
+            'blocks': self.blocks,
+            'assignee': self.assignee,
+            'comments': self.comments
+        }
+
+        return output
+
+    @property
+    def isa(self):
+        return 'issue'
+
+
+class IssueComment():
+    ''' Represent a comment for an issue '''
+
+    def __init__(
+            self, id, comment, date_created,
+            user, parent=None, edited_on=None, editor=None):
+
+        self.id = id
+        self.comment = comment
+        self.parent = parent
+        self.date_created = date_created
+        self.user = user
+        self.edited_on = edited_on
+        self.editor = editor
+
+    def to_json(self):
+        ''' Returns a dictionary representation of the issue. '''
+
+        output = {
+            'id': self.id,
+            'comment': self.comment,
+            'parent': self.parent,
+            'date_created': self.date_created.strftime('%s'),
+            'user': self.user,
+            'edited_on': self.edited_on.strftime('%s') if self.edited_on else None,
+            'editor': self.editor or None
+        }
+        return output
+
+
+class User():
+    ''' Represents a User '''
+
+    def __init__(
+            self, name, emails,
+            fullname=None):
+        self.name = name
+        self.fullname = fullname
+        self.emails = emails
+
+    def to_json(self):
+        ''' Return a representation of the User in a dictionary. '''
+
+        output = {
+            'name': self.name,
+            'fullname': self.fullname,
+            'emails': self.emails
+        }
+
+        return output
