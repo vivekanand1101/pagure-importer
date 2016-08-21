c63e119e6de2d46612ebaa5c147d69ad41aecd71
Clement Verna <cverna@tutanota.com>
2016-08-21 11:11:18 +0100

    Replaced xml-rpc login url to use json rpc requests

--- pagure_importer/commands/fedorahosted.py
+++ pagure_importer/commands/fedorahosted.py
@@ -10,20 +10,17 @@ from pagure_importer.utils.fas import FASclient
 @click.argument('project_url')
 @click.option('--tags', help="Import pagure tags:", is_flag=True)
 def fedorahosted(project_url, tags):
-    fas_username = raw_input('Enter you FAS Username: ')
-    fas_password = getpass.getpass('Enter your FAS password: ')
-    fasclient = FASclient(fas_username, fas_password,
+    username = raw_input('Enter you FAS Username: ')
+    password = getpass.getpass('Enter your FAS password: ')
+    fasclient = FASclient(username, password,
                           'https://admin.fedoraproject.org/accounts')
-
-    rpc_login = fas_username + ':' + fas_password + '@'
-    url_index = project_url.find('://')
-    rpc_url = project_url[:url_index+3] + rpc_login +\
-        project_url[url_index+3:] + '/login/xmlrpc'
+    project_url = project_url + '/login/jsonrpc'
     repos = pagure_importer.utils.display_repo()
     if repos:
         repo_index = raw_input('Choose the import destination repo (default 1) : ') or 1
         repo_name = repos[int(repo_index)-1]
-        trac_importer = importer_trac.TracImporter(rpc_url, fasclient, tags)
+        trac_importer = importer_trac.TracImporter(project_url, username,
+                                                   password, fasclient, tags)
         trac_importer.import_issues(repo_name=repo_name, repo_folder=REPO_PATH)
     else:
         click.echo('No ticket repository found. Use pgimport clone command')
