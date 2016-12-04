8cf2ebbe8199a2eedce244c7675ed1f662b137d6
Clement Verna <cverna@tutanota.com>
2016-12-04 22:06:02 +0000

    Fixing missing space in prompt
    Signed-off-by: Clement Verna <cverna@tutanota.com>

--- pagure_importer/commands/push.py
+++ pagure_importer/commands/push.py
@@ -8,7 +8,7 @@ from pagure_importer.app import app, REPO_PATH
 @click.argument('repo_name')
 def push(repo_name):
     if click.confirm('Before executing this command, you must have'
-        '"Pagure Tickets" enabled from pagure project\'s settings. Continue?'):
+        ' "Pagure Tickets" enabled from pagure project\'s settings. Continue?'):
         repo = os.path.join(REPO_PATH, repo_name)
         os.chdir(repo)
         cmd = ['git', 'push', 'origin', 'master']
